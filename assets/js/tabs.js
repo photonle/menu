2b58d4c45c9c745183750434ba357333a0c2a26c
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-11-08 21:38:40 +0000

    add the new tab script.

---
+++ assets/js/tabs.js
@@ -0,0 +1,14 @@
+windows.tabs = {
+	set: function(id){
+		$(".container > .page").hide()
+		$("#" + id).show()
+	}
+
+	bind: function(){
+		$(".sidebar li").click(function(){
+			tabs.set($(this).attr("page"))
+			$(this).siblings().removeClass("active")
+			$(this).addClass("active")
+		})
+	}
+}
