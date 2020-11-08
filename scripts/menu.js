550c514930406b8739f13f0d42b948c07675caea
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2020-11-08 18:07:15 +0000

    Intial Commit.

---
+++ scripts/menu.js
@@ -0,0 +1,37 @@
+
+var photon = {
+	init: function() {
+		photon.fix_styles();
+		photon.tab_control.init();
+	},
+	run_lua: function(cmd) {
+		console.log( "RUNLUA: " + cmd );
+	},
+	close_menu: function() {
+		photon.run_lua("ClosePhotonMenu()");
+	},
+	fix_styles: function() {
+		$(".page").css( "height", ($("body").height() - 32 - 64) );
+	},
+	tab_control: {
+		init: function() {
+			photon.tab_control.bind();
+			photon.tab_control.setTab("usage");
+		},
+		bind: function() {
+			$(".sidebar li").click(function(){
+				photon.tab_control.setTab($(this).attr("page"));
+				$(".sidebar > li").removeClass("active");
+				$(this).addClass("active");
+			});
+		},
+		setTab: function(id) {
+			$(".container > .page").hide();
+			$("#" + id).show();
+		}
+	}
+}
+
+$(document).ready(function(){
+	photon.init();
+});
